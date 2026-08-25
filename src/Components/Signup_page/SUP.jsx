ad549fbd2701998b5a94e97970cd0e5f84b525f5
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-08-25 13:29:29 +0500

    Update frontend

--- src/Components/Signup_page/SUP.jsx
+++ src/Components/Signup_page/SUP.jsx
@@ -8,6 +8,7 @@ export default function SUP() {
 
   const navigate = useNavigate();
 
+
   const [UserName, setUserName] = useState("");
   const [UserEmail, setEmail] = useState("");
   const [UserPassWord, setPassWord] = useState("");
@@ -49,79 +50,87 @@ export default function SUP() {
 
       
       <div className={SignUpcss.outercon}>
-      <div className={SignUpcss.innercon}>
+        <div className={SignUpcss.innercon}>
 
-        {/* Header */}
-        <div className={SignUpcss.inner_con_Content_Text}>
-          <h1>Welcome Back!</h1>
+
+          {/* Header */}
+            <div className={SignUpcss.inner_con_Content_Text}>
+              <h1>Welcome Back!</h1>
+            </div>
+
+
+            {/* Form */}
+            <div className={SignUpcss.innerconContent}>
+              <h2>Register</h2>
+
+              <form onSubmit={Handle_SignUp_info_new}>
+                  {/* Username */}
+                  <div className="relative mb-4">
+
+                    <input
+                      type="text"
+                      placeholder='Username'
+                      required
+                      value={UserName}
+                      onChange={(e) => setUserName(e.target.value)}
+                      className="w-full border-b border-white bg-transparent text-white pl-10 py-2 outline-none"
+                    />
+                    <i className="fa-regular fa-user absolute left-51 top-1/2 -translate-y-1/2 text-white"></i>
+                  </div>
+
+
+                  {/* Email */}
+                  <div className="relative mb-4">
+                    <input
+                      type="email"
+                      placeholder='Email'
+                      required
+                      value={UserEmail}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="w-full border-b border-white bg-transparent text-white pl-10 py-2 outline-none"
+                    />
+                    <i className="fa-regular fa-envelope absolute left-51 top-1/2 -translate-y-1/2 text-white"></i>
+                  </div>
+
+
+                  {/* Password */}
+                  <div className="relative mb-4">
+                    <input
+                      type={ShowUserPassword ? "text" : "password"}
+                      placeholder='Password'
+                      required
+                      value={UserPassWord}
+                      onChange={(e) => setPassWord(e.target.value)}
+                      className="w-full border-b border-white bg-transparent text-white pr-10 py-2 outline-none"
+                      />
+
+                    <i
+                      onClick={() => setShowUserPassword(!ShowUserPassword)}
+                      className={`fa-solid absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-white ${
+                        ShowUserPassword ? "fa-lock-open" : "fa-lock"
+                      }`}
+                      ></i>
+                  </div>
+
+
+                  {/* Submit */}
+                  <button
+                    type="submit"
+                    className="w-full py-2 mt-4 bg-blue-600 text-white rounded"
+                  >
+                    Register
+                  </button>
+
+
+                  <p className="mt-2 text-sm cursor-pointer" onClick={() => navigate("/loginpage")}>
+                    Already have an account? Sign In
+                  </p>
+
+
+                  {UserError && <p className="text-red-500 mt-2">{UserError}</p>}
+              </form>
+            </div>
         </div>
-
-        {/* Form */}
-        <div className={SignUpcss.innerconContent}>
-          <h2>Register</h2>
-
-          <form onSubmit={Handle_SignUp_info_new}>
-            {/* Username */}
-            <div className="relative mb-4">
-              <input
-                type="text"
-                placeholder='Username'
-                required
-                value={UserName}
-                onChange={(e) => setUserName(e.target.value)}
-                className="w-full border-b border-white bg-transparent text-white pl-10 py-2 outline-none"
-              />
-              <i className="fa-regular fa-user absolute left-51 top-1/2 -translate-y-1/2 text-white"></i>
-            </div>
-
-            {/* Email */}
-            <div className="relative mb-4">
-              <input
-                type="email"
-                placeholder='Email'
-                required
-                value={UserEmail}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full border-b border-white bg-transparent text-white pl-10 py-2 outline-none"
-              />
-              <i className="fa-regular fa-envelope absolute left-51 top-1/2 -translate-y-1/2 text-white"></i>
-            </div>
-
-            {/* Password */}
-            <div className="relative mb-4">
-              <input
-                type={ShowUserPassword ? "text" : "password"}
-                placeholder='Password'
-                required
-                value={UserPassWord}
-                onChange={(e) => setPassWord(e.target.value)}
-                className="w-full border-b border-white bg-transparent text-white pr-10 py-2 outline-none"
-                />
-
-              <i
-                onClick={() => setShowUserPassword(!ShowUserPassword)}
-                className={`fa-solid absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-white ${
-                  ShowUserPassword ? "fa-lock-open" : "fa-lock"
-                }`}
-                ></i>
-            </div>
-
-            {/* Submit */}
-            <button
-              type="submit"
-              className="w-full py-2 mt-4 bg-blue-600 text-white rounded"
-            >
-              Register
-            </button>
-
-            <p className="mt-2 text-sm cursor-pointer" onClick={() => navigate("/loginpage")}>
-              Already have an account? Sign In
-            </p>
-
-            {UserError && <p className="text-red-500 mt-2">{UserError}</p>}
-          </form>
-        </div>
-      </div>
       </div>
  
   </>
