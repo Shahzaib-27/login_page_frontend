b46b5bcba476f2c62588b5ef234ae2d02373bc9b
Shahzaib Shahid <shahzaibshahidr10@gami.com>
2026-08-26 16:57:16 +0500

    code push

--- src/Components/Login_Page/LP.jsx
+++ src/Components/Login_Page/LP.jsx
@@ -20,7 +20,7 @@ export default function LP() {
 
     try {
       
-       const response = await axios.post(
+        const response = await axios.post(
           `${import.meta.env.VITE_API_URL}/user/login`,
           {
             name: user,
--- src/Components/Signup_page/SUP.jsx
+++ src/Components/Signup_page/SUP.jsx
@@ -23,7 +23,7 @@ export default function SUP() {
 
         try {
 
-            const response = await axios.post(
+              const response = await axios.post(
               `${import.meta.env.VITE_API_URL}/user/register`,
               {
                 name: UserName,
